37249b7298cb1bb1a7f15746437cff43165427e0
ashokvishwanath <ashokvishwanath@outlook.com>
2025-06-27 16:45:10 +0530

    updated Service B

--- services/service-b/main.go
+++ services/service-b/main.go
@@ -1,1 +1,15 @@
-# Placeholder for main.go
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello from Service B!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":5000", nil)
+}
